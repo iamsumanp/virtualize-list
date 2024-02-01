28fb63ea57bc2c268443b28072140df16dc54dbf
Suman <sumanpok9@gmail.com>
2024-02-02 01:53:27 +0545

    fix : add virtualization disable switch to render and mount all properties in list

--- src/components/VirtualizedList/VirtulizedList.tsx
+++ src/components/VirtualizedList/VirtulizedList.tsx
@@ -6,8 +6,13 @@ import { throttle } from 'lodash';
 const VirtulizedList = () => {
   const [containerRef, size] = useElementSize();
   const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [isVirtualizationEnabled, setIsVirtualizationEnabled] = useState(true);
 
   const { height: containerHeight } = size;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsVirtualizationEnabled(event.target.checked);
+  };
 
   const onScroll = useMemo(
     () =>
@@ -29,12 +34,28 @@ const VirtulizedList = () => {
 
     const endIndex = Math.min(
       Math.ceil((scrollPosition + containerHeight) / 40 - 1), //adjust the index of the array
-      Array.from(new Array(300000)).length - 1
+      Array.from(new Array(15000)).length - 1
     );
 
-    return Array.from(new Array(300000))
-      .slice(startIndex, endIndex)
-      .map((item, index) => (
+    if (isVirtualizationEnabled) {
+      return Array.from(new Array(15000))
+        .slice(startIndex, endIndex)
+        .map((item, index) => (
+          <li
+            className="listyle"
+            style={{
+              position: 'absolute',
+              top: (startIndex + index) * 40 + index * 5,
+              height: 40,
+              left: 0,
+              right: 0,
+            }}
+          >
+            {generateRandomText()}
+          </li>
+        ));
+    } else {
+      return Array.from(new Array(15000)).map((item, index) => (
         <li
           className="listyle"
           style={{
@@ -48,6 +69,7 @@ const VirtulizedList = () => {
           {generateRandomText()}
         </li>
       ));
+    }
   };
   const generateRandomText = () => {
     const alphabets =
@@ -65,14 +87,32 @@ const VirtulizedList = () => {
     return randomText;
   };
   return (
-    <ul
-      className="container"
-      ref={containerRef}
-      onScroll={onScroll}
-      style={{ position: 'relative' }}
-    >
-      {visibleChidren()}
-    </ul>
+    <div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+        {' '}
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isVirtualizationEnabled}
+            onChange={handleChange}
+          />
+          <span className="slider round"></span>
+        </label>
+        <span>
+          {isVirtualizationEnabled
+            ? 'Virtualization Enabled'
+            : 'Virtualization Disabled'}
+        </span>
+      </div>
+      <ul
+        className="container"
+        ref={containerRef}
+        onScroll={onScroll}
+        style={{ position: 'relative' }}
+      >
+        {visibleChidren()}
+      </ul>
+    </div>
   );
 };
 
